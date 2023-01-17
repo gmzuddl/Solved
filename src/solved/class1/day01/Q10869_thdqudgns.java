2cbeb8bede380ed7bc6252aa87c3f04290d5147c
HUN\Hun <thdqudgns9612@gmail.com>
2023-01-17 21:44:07 +0900

    name_change

--- src/solved/class1/day01/Q10869_thdqudgns.java
+++ src/solved/class1/day01/Q10869_thdqudgns.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Solved_thdqudgns9612_10869 {
+public class Q10869_thdqudgns {
 	public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
